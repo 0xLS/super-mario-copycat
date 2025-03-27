90c62a6ab3e0dc6e56f88cdcf2e7987989593ca7
unknown <jonholm06@gmail.com>
2025-03-27 18:29:06 +0100

    added menu

--- scenes/strawberry.gd
+++ scenes/strawberry.gd
@@ -4,5 +4,9 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == 'CharacterBody2D'):
-		queue_free()
 		gameManager.addPoint()
+		var tween = get_tree().create_tween() 
+		var tween2 = get_tree().create_tween() 
+		tween.tween_property(self, "position", position - Vector2(0, 35), 0.35) 
+		tween2.tween_property(self, "modulate:a", 0, 0.2) 
+		tween.tween_callback(queue_free)
