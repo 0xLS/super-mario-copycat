1ff4b60bad1b34d033ed2b719a82c31d7ec1d10a
0xLS <zerodna1@gmail.com>
2025-03-27 16:06:31 +0100

    Semi-Dynamic scene changer for entering end area

--- scenes/end.gd
+++ scenes/end.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+# pass in the current file name (e.g level1.tscn)
+func isCurrentScene(scene: String):
+	var currentScene = get_tree().current_scene
+	return currentScene.scene_file_path.contains(scene)
+
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == 'CharacterBody2D'):
-		get_tree().change_scene_to_file("res://scenes/level2.tscn")
+		if(isCurrentScene('level1')):
+			get_tree().change_scene_to_file("res://scenes/level2.tscn")
