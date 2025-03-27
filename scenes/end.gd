06c48649efbb177f1e9fb8fc1674d54a1a5cc01e
0xLS <zerodna1@gmail.com>
2025-03-27 17:18:53 +0100

    Proper scene changing using exported vars & packed scenes

--- scenes/end.gd
+++ scenes/end.gd
@@ -1,11 +1,7 @@
 extends Area2D
 
-# pass in the current file name (e.g level1.tscn)
-func isCurrentScene(scene: String):
-	var currentScene = get_tree().current_scene
-	return currentScene.scene_file_path.contains(scene)
+@export var target_level : PackedScene
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == 'CharacterBody2D'):
-		if(isCurrentScene('level1')):
-			get_tree().change_scene_to_file("res://scenes/level2.tscn")
+		get_tree().change_scene_to_packed(target_level)
